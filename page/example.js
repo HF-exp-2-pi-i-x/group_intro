2ce80eace0908dd557c40826a569ca208c73aad0
HF-exp-2-pi-i-x <hanlolccc@gmail.com>
2023-08-17 14:03:46 +0800

    change url of D3 mp4

--- page/example.js
+++ page/example.js
@@ -4,7 +4,7 @@ const example = `
         <p>This group represents symmetry of equilateral triangle by rotation and reflection. In the animation below, <span class="math inline">$$\\rho$$ represents rotation by $$120$$&deg; and <span class="math inline">$$r$$</span> represents reflection along the axis that bisects triangle. </span></p>
         <p>The symmetry group follows all four axioms of group and the binary operator for this group is composition <span class="math inline">$$\\circ$$.</span>Group operation is defined by <span class="math inline">$$a \\circ b$$, which means apply $$b$$ on triangle then apply $$a$$</span></p>
         <video controls>
-            <source src="../assets/videos/D3.mp4" type="video/mp4">
+            <source src="https://github.com/HF-exp-2-pi-i-x/group_intro/blob/main/assets/videos/D3.mp4" type="video/mp4">
         </video>
         <p>The above animation is built by Manim library. Source code is <a href="https://github.com/HF-exp-2-pi-i-x/3b1b_manim/blob/main/group.py" target="_blank"><img src="../assets/icons/new-tab.svg">&nbsp;here</a></p>
     </div>
